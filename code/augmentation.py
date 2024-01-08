3cef2bf11e80153ac69ed1817eaba38609283d10
BJH9 <tfzangin9@gmail.com>
2024-01-08 06:29:31 +0000

    [fix]:  readtimeout 에러 해결

--- code/augmentation.py
+++ code/augmentation.py
@@ -1,9 +1,13 @@
 import pandas as pd
+import numpy as np
 from googletrans import Translator
 from tqdm.auto import tqdm
+import time
 
 train_path = './dataset/train/train.csv'
 train_df = pd.read_csv(train_path)
+train_df_len = len(train_df)
+
 translator = Translator()
 
 def google_ko2en2ko(ko_text, translator):
@@ -11,11 +15,18 @@ def google_ko2en2ko(ko_text, translator):
     en_ko_result = translator.translate(ko_en_result, dest = 'ko').text
     return en_ko_result
 
-sen_list = train_df['sentence'] 
 
-for idx, sentence in enumerate(tqdm(sen_list)):
-    result =  google_ko2en2ko(sentence, translator)
-    train_df.loc[idx,'sentence'] = result
+sen1_list = train_df['sentence'] 
+
+for idx, sentence in enumerate(tqdm(sen1_list)):
+     while True:
+        try:
+            result = google_ko2en2ko(sentence, translator)
+            train_df.loc[idx, 'sentence'] = result
+            break  
+        except Exception as e:
+            print(f"번 째 index에서 에러 발생 {idx} {e}")
+            time.sleep(1)  # 1초의 지연 후 다시 시도
 
 train_df.to_csv('./dataset/train/trans_train.csv', index=False)
 
